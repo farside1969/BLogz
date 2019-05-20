57739aa37d2af258c99a459e91715cd11ffa08ae
farside1969 <farside1969@hotmail.com>
2019-05-20 09:04:27 -0500

    temp bypass hash

--- models.py
+++ models.py
@@ -13,15 +13,22 @@ class Blog(db.Model):
         self.body = body
         self.owner = owner
 
+    def __repr__(self):
+        return '<Blog %r>' % self.title
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), unique=True)
-    pw_hash = db.Column(db.String(128))
+    password = db.Column(db.String(128))
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
-        self.pw_hash = make_pw_hash(password)
+        self.password = password
+
+#    def __init__(self, username, password):
+#        self.username = username
+#        self.pw_hash = make_pw_hash(password)
 
     def __repr__(self):
-        return str(self.username)
+        return '<User %r>' % self.username
